bd9bce7648f097e271d9fea679fead72c7a9835f
couchpotatoAni <ananyanov22@gmail.com>
2026-02-19 15:54:28 +0530

    pattern4.java

--- pattern4.java
+++ pattern4.java
@@ -1,3 +1,20 @@
 public class pattern4 {
-    
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 0; i < n; i++) {
+          System.out.print("*");  
+        }
+        System.out.println();
+        for( int i=0; i<n-2;i++){
+            System.out.println("*");
+            for (int j = 0; j < n-2; j++) {
+                System.out.print("");
+                
+            }
+            System.out.println("*");
+        }
+        for(int i=0; i<n;i++){
+            System.out.print("*");
+        }
+    }
 }
